a3167d2e1dce57aaac97e9e4c01c27aab37838a7
Irina-Koksharova <biodgerelo@ukr.net>
2021-02-12 20:28:38 +0200

    Adds commander and function invokeAction

--- contacts.js
+++ contacts.js
@@ -1,16 +1,18 @@
 import * as fs from 'fs/promises'
 import * as path from 'path'
 import shortid from 'shortid';
+import createDirname from './lib/dirname.js'
 
-const contactsPath = path.join('db', 'contacts.json')
+const __dirname = createDirname(import.meta.url)
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json')
 
 async function listContacts() {
   try {
       const data = await fs.readFile(contactsPath)
       const contacts = JSON.parse(data)
-      contacts.length > 0
-          ? console.table(contacts)
-          : console.log('You have no contacts in your list')
+      console.log('Here is your contacts list')
+      console.table(contacts)
   } catch (error) {
       console.log(error.message)
     }
@@ -19,7 +21,7 @@ async function listContacts() {
 async function getContactById(contactId) {
     try {
         const data = await fs.readFile(contactsPath)
-        const selectedContact = JSON.parse(data).find(({ id }) => id === contactId)
+        const selectedContact = JSON.parse(data).find(({ id }) => id.toString() === contactId)
         selectedContact
             ? console.table(selectedContact)
             : console.log('You have no such contact')
@@ -35,9 +37,10 @@ async function removeContact(contactId) {
         if (contacts.find(({ id }) => id === contactId)) {
             const updatedContacts = contacts.filter(({ id }) => id !== contactId)
             await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2))
-            console.log('This operation was successful')
+            console.log('Delete operation was successful')
+            listContacts()
         } else {
-            console.log('This operation cannot be performed')
+            console.log(`Delete operation cannot be performed. There is no user with id ${contactId}`)
         }
     } catch (error) {
         console.log(error.message)
@@ -58,7 +61,7 @@ async function addContact(name, email, phone) {
         if (!includesContact) {
             const newList = [...contacts, newContact]
             await fs.writeFile(contactsPath, JSON.stringify(newList, null, 2))
-            console.log(`Contact ${newContact.name} has been added to your list`)
+            console.log(`${newContact.name} has been added to your list`)
             console.table(newList)
         } else {
             console.log(`${newContact.name} is already in your contacts`)
--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-import {listContacts, getContactById, removeContact, addContact} from './contacts.js'
+import program from './lib/commander.js'
+import {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+} from './contacts.js'
 
-// listContacts()
-// getContactById(13)
-// removeContact(2)
-// removeContact(3)
-// removeContact(4)
-addContact(111, 111, 111)
-// addContact(222, 222, 222)
-// addContact(333, 333, 333)
+program.parse(process.argv);
+const options = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      listContacts()
+      break;
+
+    case 'get':
+      getContactById(id)
+      break;
+
+    case 'add':
+      addContact(name, email, phone)
+      break;
+
+    case 'remove':
+      removeContact(id)
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(options);
+
 
 
 
